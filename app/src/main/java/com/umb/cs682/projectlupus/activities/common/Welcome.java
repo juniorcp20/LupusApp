497d60bafde2b795b48b4f702a8a5e3fbb6a6115
Nithya Konda <nithya.konda001@umb.edu>
2015-03-05 15:38:29 -0500

    Added Welcome page

--- app/src/main/java/com/umb/cs682/projectlupus/activities/common/Welcome.java
+++ app/src/main/java/com/umb/cs682/projectlupus/activities/common/Welcome.java
@@ -2,8 +2,10 @@ package com.umb.cs682.projectlupus.activities.common;
 
 import com.umb.cs682.projectlupus.R;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Welcome extends Activity {
 
@@ -11,5 +13,12 @@ public class Welcome extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_welcome);
+        ActionBar actionBar = getActionBar();
 	}
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.m_welcome, menu);
+        return true;
+    }
 }

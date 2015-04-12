e28c8963aea2d3a7fb2182c21a59dcf56dde1d18
Nithya Konda <nithya.konda001@umb.edu>
2015-04-12 11:33:06 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit e08225c975f6a1c0eb7125a91d6a09a3649ef6ad, reversing
    changes made to 63aa9f37eddd3a7efe621c0e116784673f316240.

--- app/src/main/java/com/umb/cs682/projectlupus/activities/activitySense/ActivitySense.java
+++ app/src/main/java/com/umb/cs682/projectlupus/activities/activitySense/ActivitySense.java
@@ -1,175 +1,56 @@
 package com.umb.cs682.projectlupus.activities.activitySense;
 
+import com.umb.cs682.projectlupus.R;
+import com.umb.cs682.projectlupus.activities.medicineAlert.MedicineAlert;
+import com.umb.cs682.projectlupus.util.Constants;
+
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.ToggleButton;
-
-import com.umb.cs682.projectlupus.R;
-import com.umb.cs682.projectlupus.activities.medicineAlert.MedicineAlert;
-import com.umb.cs682.projectlupus.config.AppConfig;
-import com.umb.cs682.projectlupus.service.ActivitySenseService;
-import com.umb.cs682.projectlupus.util.Constants;
-import com.umb.cs682.projectlupus.util.SharedPreferenceManager;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ActivitySense extends Activity {
-    private static final String TAG = "projectlupus.activities";
+    private String parent = null;
 
-    private boolean isInit = SharedPreferenceManager.getBooleanPref(Constants.IS_FIRST_RUN);
-    private String DEFAULT_SENSITIVITY = "1";
-    private boolean isOn = false;
-
-    private static ToggleButton startStopButton = null;
-    private static TextView text = null;
-
-    private static ArrayList<String> sensArrayList = null;
-    private static ArrayAdapter<CharSequence> modesAdapter = null;
-
-    private ActivitySenseService service = AppConfig.getActivitySenseService();
-    private static Handler handler = null;
-
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.a_activity_sense);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.a_activity_sense);
+        parent = getIntent().getStringExtra(Constants.PARENT_ACTIVITY_NAME);
+        Log.i(Constants.ACTIVITY_SENSE, parent);
         ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        if(isInit){
+	    actionBar.setDisplayHomeAsUpEnabled(true);
+        if(parent.equals(Constants.MOOD_ALERT)){
             actionBar.setTitle(R.string.title_init_activity_sense);
         }else {
             actionBar.setTitle(R.string.title_activity_sense);
         }
-        startStopButton = (ToggleButton) this.findViewById(R.id.tb_start_stop);
-        startStopButton.setOnCheckedChangeListener(startStopListener);
+	}
 
-        String sensStr = String.valueOf(DEFAULT_SENSITIVITY);
-        int idx = 0;
-
-        if (sensArrayList == null) {
-            String[] sensArray = getResources().getStringArray(R.array.sensitivity);
-            sensArrayList = new ArrayList<String>(Arrays.asList(sensArray));
-        }
-        if (sensArrayList.contains(sensStr)) {
-            idx = sensArrayList.indexOf(sensStr);
-        }
-
-        Spinner sensSpinner = (Spinner) findViewById(R.id.sp_sensitivity);
-        modesAdapter = ArrayAdapter.createFromResource(this, R.array.sensitivity, android.R.layout.simple_spinner_item);
-        modesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sensSpinner.setOnItemSelectedListener(sensListener);
-        sensSpinner.setAdapter(modesAdapter);
-        sensSpinner.setSelection(idx);
-
-        text = (TextView) this.findViewById(R.id.tv_steps);
-        setHandler();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if(isInit) {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+        if(parent.equals(Constants.MOOD_ALERT)) {
             getMenuInflater().inflate(R.menu.m_action_next, menu);
         }
-        return true;
-    }
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_next) {
-            SharedPreferenceManager.setBooleanPref(TAG,Constants.ACTIVITY_SENSE_SETTING,isOn);
-            if(isOn)
-                SharedPreferenceManager.setIntPref(TAG, Constants.SENSITIVITY_VALUE, service.getSensitivity());
-            next();
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_next) {
+			next();
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        service.onStartPedometer();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        service.onPausePedometer();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        service.unsetHandler();
-        Log.d(TAG, "unsetHandler");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        setHandler();
-        Log.d(TAG,"setHandler");
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }
-
-    private OnItemSelectedListener sensListener = new OnItemSelectedListener() {
-        @Override
-        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-            CharSequence seq = modesAdapter.getItem(arg2);
-            String sensString = String.valueOf(seq);
-            if (sensString != null) {
-                service.setSensitivity(Integer.parseInt(sensString));
-            }
-        }
-        @Override
-        public void onNothingSelected(AdapterView<?> arg0) {
-            // Ignore
-        }
-    };
-
-    private OnCheckedChangeListener startStopListener = new OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            isOn = isChecked;
-            service.startStopPedometer(isChecked);
-        }
-    };
-
-    private void setHandler() {
-        Log.d(TAG,"setHandler");
-        service.setHandler(new Handler() {
-
-            public void handleMessage(Message msg) {
-                int current = msg.arg1;
-                text.setText("Steps = " + current);
-            }
-        });
-    }
     //To identify the parent activity at run-time and provide up navigation accordingly
     @Override
     public Intent getParentActivityIntent(){
@@ -180,7 +61,7 @@ public class ActivitySense extends Activity {
 
     public void next(){
         Intent intent = new Intent();
-        //intent.putExtra(Constants.PARENT_ACTIVITY_NAME, Constants.ACTIVITY_SENSE);
+        intent.putExtra(Constants.PARENT_ACTIVITY_NAME, Constants.ACTIVITY_SENSE);
         startActivity(intent.setClass(this, MedicineAlert.class));
     }
 }
--- app/src/main/java/com/umb/cs682/projectlupus/util/Constants.java
+++ app/src/main/java/com/umb/cs682/projectlupus/util/Constants.java
@@ -3,8 +3,7 @@ package com.umb.cs682.projectlupus.util;
 /**
  * Created by Nithya Konda on 2/25/2015.
  */
-public final class Constants {
-    private Constants(){}
+public class Constants {
     //Keys
     public static String PARENT_ACTIVITY_NAME = "ParentActivityName";
     public static String IS_INIT = "Is the process an Initialization";
@@ -16,8 +15,4 @@ public final class Constants {
     public static String ACTIVITY_SENSE = "Activity_Sense";
     public static String MEDICINE_ALERT = "Medicine_Alert";
 
-    //Shared Prefs Keys
-    public static String ACTIVITY_SENSE_SETTING = "Activity_Sense_Setting";
-    public static String IS_FIRST_RUN = "Is_First_Run";
-    public static String SENSITIVITY_VALUE = "Sensitivity_Value";
 }

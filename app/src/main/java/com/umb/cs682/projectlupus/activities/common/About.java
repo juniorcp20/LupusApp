8d5b011531cd88e6b6fd568b8fe6ce3b67f5ce53
Lipsaray <lipsa.ray001@umb.edu>
2015-05-08 19:29:03 -0400

    cosmetic changes

--- app/src/main/java/com/umb/cs682/projectlupus/activities/common/About.java
+++ app/src/main/java/com/umb/cs682/projectlupus/activities/common/About.java
@@ -11,6 +11,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,10 +20,14 @@ import android.widget.TextView;
 
 public class About extends Activity {
 
+    TextView t2;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.a_about);
+        t2 = (TextView) findViewById(R.id.about_text);
+        t2.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 
 	@Override
